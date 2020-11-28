2c247b8a10972a5377d1ac2c13d54a6b3efb3933
happyuser100 <fleidmos@gmail.com>
2020-11-28 19:10:33 +0200

    Gif .Net Core 3.1 Server

---
+++ GiphyAPI/GiphyAPI/Controllers/GiphyController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GiphyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]  
+    //[ResponseCache(Duration = 60)]
+    [ResponseCache(CacheProfileName = "default")]
+    public class GiphyController : ControllerBase
+    {
+        string _BaseUrl = "";
+        string _API_KEY = "";
+        IConfiguration configuration;               
+
+        public GiphyController(IConfiguration iConfig)
+        {
+            configuration = iConfig;
+            _BaseUrl = configuration.GetSection("MySettings").GetSection("BaseUrl").Value;
+            _API_KEY = configuration.GetSection("MySettings").GetSection("API_KEY").Value;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+
+        [HttpGet]
+        [Route("SearchGiphy/{search}")]
+        public string SearchGiphy(string search)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_BaseUrl + $"search?q={search}&api_key={_API_KEY}&limit=5");
+            var data = client.GetAsync("").Result.Content.ReadAsStringAsync().Result;
+            return data;
+        }
+
+        [HttpGet]
+        [Route("SearchTrend")]
+        public string SearchTrend()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_BaseUrl + $"trending?api_key={_API_KEY}&limit = 5");
+            var data = client.GetAsync("").Result.Content.ReadAsStringAsync().Result;
+            return data;
+        }
+
+    }
+}
---
+++ GiphyAPI/GiphyAPI/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GiphyAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "AllowSpecificOrigin",
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:4200");
+                                  });
+            });
+            services.AddResponseCaching();
+            services.AddMvc(options =>
+            {
+                options.CacheProfiles.Add("default", new CacheProfile
+                {
+                    Duration = 30,
+                    Location = ResponseCacheLocation.Any,
+                    NoStore = false
+                });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseCors("AllowSpecificOrigin");
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseResponseCaching();
+        }
+    }
+}
